2f223aa513fdabb2e70fd75211a872ce20387756
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2025-06-01 16:27:55 +0200

    Use numpy as the docstring style

--- finml/interface.py
+++ finml/interface.py
@@ -4,45 +4,46 @@ import numpy.typing as npt
 
 
 class TimeSeriesModel(Protocol):
-    """
-    An adapter for time series models.
-    """
+    """An adapter for time series models."""
 
-    def __init__(self, hyperparameters: dict = None) -> npt.NDArray[Any]:
-        """
-        Initialize the time series model.
-        
-        Parameters:
-        - hyperparameters: A dictionary of hyperparameters for the model.
+    def __init__(self, hyperparameters: dict = None):
+        """Initialize the time series model.
+
+        Parameters
+        ----------
+        hyperparameters : dict, optional
+            A dictionary of hyperparameters for the model, by default None.
         """
         self.hyperparameters = hyperparameters 
         ...
 
-    def fit(self, data, **kwargs) -> None:
-        """
-        Fit the model to the provided time series data.
+    def fit(self, data: npt.NDArray[Any], **kwargs) -> None:
+        """Fit the model to the provided time series data.
 
-        Parameters:
-        - data: The time series data to fit the model to.
-        - kwargs: Additional parameters for model fitting.
+        Parameters
+        ----------
+        data : npt.NDArray[Any]
+            The time series data to fit the model to.
         """
         ...
 
-    def predict(self, steps, past_data=None) -> npt.NDArray[Any]:
-        """
-        Predict future values based on the fitted model and the
+    def predict(self, steps: int, past_data=None) -> npt.NDArray[Any]:
+        """Predict future values based on the fitted model and the
         past data provided to it.
 
         `past_data` is assumed to be a contiguous sequence of points.
         The predictions are then made following the last point in `past_data`.
 
-        If `past_data` is not provided...
+        Parameters
+        ----------
+        steps : int
+            The number of future time steps to predict.
+        past_data : _type_, optional
+            Historical data to use for prediction, by default None
 
-        Parameters:
-        - steps: The number of future time steps to predict.
-        - past_data: Optional historical data to use for prediction.
-
-        Returns:
-        - Predicted values for the specified number of steps.
+        Returns
+        -------
+        npt.NDArray[Any]
+            The model predictions for the next `steps` time steps.
         """
         ...
